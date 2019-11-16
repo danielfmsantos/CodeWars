f321e336c5ace78c5e85cd8e346cd5b936ba45f9
Daniel Santos <santos.m.f.daniel@gmail.com>
2019-11-16 20:17:18 +0000

    WeightSort

---
+++ src/main/java/kyu5/WeightSort.java
@@ -0,0 +1,19 @@
+package kyu5;
+
+/**
+ * https://www.codewars.com/kata/55c6126177c9441a570000cc
+ * @author Daniel Santos
+ *
+ */
+public class WeightSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static String orderWeight(String strng) {
+		// your code
+	}
+
+}
